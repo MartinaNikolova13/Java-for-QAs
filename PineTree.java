0ab556674fca13dd535c67bd869bc642e66c6e7a
MartinaNikolova13 <92273090+MartinaNikolova13@users.noreply.github.com>
2025-08-09 21:25:04 +0300

    Add files via upload

---
+++ PineTree.java
@@ -0,0 +1,11 @@
+package Lecture1;
+
+public class PineTree {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" ******");
+        System.out.println("********");
+        System.out.println("  | |");
+    }
+}
