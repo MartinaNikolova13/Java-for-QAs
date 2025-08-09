f4ae6a891fc2936b36318c3d9efffbc1f9a0eebe
MartinaNikolova13 <92273090+MartinaNikolova13@users.noreply.github.com>
2025-08-09 21:18:21 +0300

    Add files via upload

---
+++ PerimeterTriangle.java
@@ -0,0 +1,11 @@
+package Lecture1;
+
+public class PerimeterTriangle {
+    public static void main(String[] args) {
+        Double sideA = 4.0;
+        Double sideB = 5.0;
+        Double sideC = 3.0;
+        Double perimeter = sideA + sideB + sideC;
+        System.out.println("The perimeter is: " + perimeter);
+    }
+}
