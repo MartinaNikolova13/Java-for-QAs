6330f81dcb8c2fb83340e5cb0bd4d05126e67c83
MartinaNikolova13 <92273090+MartinaNikolova13@users.noreply.github.com>
2025-08-09 23:30:23 +0300

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,18 @@
+package Lecture2;
+import java.util.Scanner;
+
+    public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        if (isLeap) {
+            System.out.println("The year is leap.");
+        } else {
+            System.out.println("The year is not leap.");
+        }
+        scanner.close();
+    }
+}
