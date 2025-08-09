85da9e04a12a580c42b1c02e7a58bb31e0209322
MartinaNikolova13 <92273090+MartinaNikolova13@users.noreply.github.com>
2025-08-09 21:22:23 +0300

    AreaTriangle.java

--- AreaTriangle.java
+++ AreaTriangle.java
@@ -1,4 +1,11 @@
 package Lecture1;
 
 public class AreaTriangle {
+    public static void main(String[] args) {
+        Double sideA = 4.0;
+        Double sideB = 5.0;
+        Double sideC = 3.0;
+        Double triangleArea= (sideA + sideB + sideC) / 2;
+        System.out.println("The area is: " + triangleArea);
+    }
 }
