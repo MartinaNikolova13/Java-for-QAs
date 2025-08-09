e0f6dc0a70daa88c3543813f7b4ae9e44d3185b6
MartinaNikolova13 <92273090+MartinaNikolova13@users.noreply.github.com>
2025-08-09 21:14:03 +0300

    Add files via upload

---
+++ MyNames.java
@@ -0,0 +1,10 @@
+package Lecture1;
+
+public class MyNames {
+    public static void main(String[] args) {
+        String firstName = "Martina";
+        String secondName = "Kamenova";
+        String lastName = "Nikolova";
+        System.out.println(firstName + " " + secondName + " "+ lastName);
+    }
+}
